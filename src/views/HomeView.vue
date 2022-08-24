7d52cb2e8a57d3a4bb411976b06a1b5a0840888b
Dan Cross <dancross96@gmail.com>
2022-08-24 12:41:52 -0500

    added index action to frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,31 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      shows: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexShows()
+  },
+  methods: {
+    indexShows: function () {
+      console.log("yo")
+      axios.get("/shows").then(response => {
+        console.log(response.data)
+        this.shows = response.data
+      })
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="show in shows">
+      <p> {{ show.name }} </p>
+      <p>{{ show.episodes }}</p>
+    </div>
   </div>
 </template>
 
